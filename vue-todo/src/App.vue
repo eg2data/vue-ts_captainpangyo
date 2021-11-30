54d60a2315fd1eab2878a1037b0f5f96bb92ae13
eg2data <eg2data@gmail.com>
2021-11-30 18:21:23 +0900

    30th November, save for changing Mac.

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>blank_test</h1>
+    <TodoInput :test="todoText" ></TodoInput>
+  </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+    import TodoInput from "@/components/TodoInput.vue";
+
+    export default Vue.extend ({
+      components: {TodoInput},
+      data() {
+        return {
+          todoText: "reset"
+        }
+      },
+      methods: {
+
+      },
+    })
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input id="todo-input" type="text" :value="test" @input="updateTodoText" />
+    <button type="button">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend ({
+      props: ['test'],
+      methods: {
+        updateTodoText(event) {
+          event.target.value;
+
+        }
+      }
+
+    })
+</script>
+
+<style scoped>
+
+</style>
